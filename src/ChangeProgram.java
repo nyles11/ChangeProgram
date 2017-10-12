397aecc0ceaf64a6a9549e6da18f7dc9d99042e8
Rob Hardy <robhardy777@gmail.com>
2017-10-12 00:24:16 -0400

    second commit

---
+++ src/ChangeProgram.java
@@ -0,0 +1,16 @@
+public class ChangeProgram {
+    public static void main(String[] args) {
+        int x;
+        double z;
+        double y;
+
+        x = 5;
+        y = 9.5;
+
+        z = (x * y);
+
+        System.out.println("The product is " + z);
+    }
+}
+
+
